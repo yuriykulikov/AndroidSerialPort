98888400a8857c59ee0273ee38f2452d79c5d4e4
Kate <kate.lyavinskova@gmail.com>
2013-07-03 21:51:18 +0200

    Implemented Media Key and Volume key handling

--- src/com/github/androidserialport/KeyEventAnalyzer.java
+++ src/com/github/androidserialport/KeyEventAnalyzer.java
@@ -1,33 +1,76 @@
 package com.github.androidserialport;
 
-import com.github.androidutils.logger.Logger;
-
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.media.AudioManager;
 import android.view.KeyEvent;
+
+import com.github.androidutils.logger.Logger;
 
 public class KeyEventAnalyzer extends BroadcastReceiver {
     Context context;
+    int keyEventAction = 0;
+    int keyEventCode = 0;
 
-    public void sendKeyEvent() {
-
-        Intent downIntent = new Intent(Intent.ACTION_MEDIA_BUTTON, null);
-        KeyEvent downEvent = new KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_MEDIA_PLAY_PAUSE);
-        downIntent.putExtra(Intent.EXTRA_KEY_EVENT, downEvent);
-        context.sendOrderedBroadcast(downIntent, null);
-
-        Intent upIntent = new Intent(Intent.ACTION_MEDIA_BUTTON, null);
-        KeyEvent upEvent = new KeyEvent(KeyEvent.ACTION_UP, KeyEvent.KEYCODE_MEDIA_PLAY_PAUSE);
-        upIntent.putExtra(Intent.EXTRA_KEY_EVENT, upEvent);
-        context.sendOrderedBroadcast(upIntent, null);
+    private void sendKeyEvent() {
+        Intent buttonPressedIntent = new Intent(Intent.ACTION_MEDIA_BUTTON, null);
+        KeyEvent event = new KeyEvent(keyEventAction, keyEventCode);
+        buttonPressedIntent.putExtra(Intent.EXTRA_KEY_EVENT, event);
+        context.sendOrderedBroadcast(buttonPressedIntent, null);
     }
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        Logger.getDefaultLogger().d(intent.toString());
-        
+        this.context = context;
+        String string = intent.getStringExtra(ReadingService.EXTRA_STRING);
+        Logger.getDefaultLogger().d("Intent contains: " + string);
 
+        if (string.contains("VOLUME")) {
+            onVolumeKeyEvent(string);
+        } else {
+            onMediaKeyEvent(string);
+        }
+    }
+
+    /**
+     * @param string
+     */
+    private void onVolumeKeyEvent(String string) {
+        AudioManager audio = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
+        if (string.contains("ACTION_UP")) {
+            if (string.contains("KEYCODE_VOLUME_DOWN")) {
+                audio.adjustStreamVolume(AudioManager.STREAM_MUSIC, AudioManager.ADJUST_LOWER,
+                        AudioManager.FLAG_SHOW_UI);
+            }
+            if (string.contains("KEYCODE_VOLUME_UP")) {
+                audio.adjustStreamVolume(AudioManager.STREAM_MUSIC, AudioManager.ADJUST_RAISE,
+                        AudioManager.FLAG_SHOW_UI);
+            }
+        }
+    }
+
+    /**
+     * @param string
+     */
+    private void onMediaKeyEvent(String string) {
+        if (string.contains("ACTION_DOWN")) {
+            keyEventAction = KeyEvent.ACTION_DOWN;
+        }
+        if (string.contains("ACTION_UP")) {
+            keyEventAction = KeyEvent.ACTION_UP;
+        }
+
+        if (string.contains("KEYCODE_MEDIA_PLAY_PAUSE")) {
+            keyEventCode = KeyEvent.KEYCODE_MEDIA_PLAY_PAUSE;
+        }
+        if (string.contains("KEYCODE_MEDIA_NEXT")) {
+            keyEventCode = KeyEvent.KEYCODE_MEDIA_NEXT;
+        }
+        if (string.contains("KEYCODE_MEDIA_PREVIOUS")) {
+            keyEventCode = KeyEvent.KEYCODE_MEDIA_PREVIOUS;
+        }
+        sendKeyEvent();
     }
 
 }
